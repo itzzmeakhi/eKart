4652c12128ec8536cfe1a6ec55eb3b89ce899d15
Mallidi Akhil Reddy <mallidi.akhilreddy@publicissapient.com>
2022-10-04 14:18:11 +0530

    feat: added BE routes for serving static data

---
+++ kart-be/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false
+  }
+}, {
+  timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- kart-fe/src/components/ProductList/ProductList.js
+++ kart-fe/src/components/ProductList/ProductList.js
@@ -1,12 +1,21 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
+import axios from 'axios';
 
 import Product from '../Product/Product';
 
-import products from '../../products';
-
 const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    console.log('In product list useEffect')
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <Row>
